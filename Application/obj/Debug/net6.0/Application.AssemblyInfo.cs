ea956a09af5f4f929d84ca383cac249210a3b32c
Kenenth Castillo <gustavo23castillo23@gmail.com>
2025-05-10 11:02:03 -0600

    Actualizar el .gitignore y remover el .sln

--- Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea507842f327c70c461144680a7d9477b5d3b3e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a399974930fc9b4ad3d80eee74dfc2dc72c8b0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
